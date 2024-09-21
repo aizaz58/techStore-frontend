575e3335760df2e10b8773051a59cd013ca42765
Aizaz <aizaz58@yahoo.com>
2024-09-21 12:59:09 +0500

    deployment errors resolved

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://techstore-backend-production.up.railway.app",
 			},
 		},
 	},
