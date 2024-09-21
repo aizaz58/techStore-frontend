de135cadb63ec7e2c0eb277cabf4f686c5c1cf19
Aizaz <aizaz58@yahoo.com>
2024-09-21 13:18:57 +0500

    deployment errors resolved

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://techstore-backend-production.up.railway.app/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "https://techstore-backend-production.up.railway.app/api" : "https://techstore-backend-production.up.railway.app/api",
 	withCredentials: true, // send cookies to the server
 });
 
